38d184d15ba4c57762a0d65fbc313c089d2e3414
Filipe Abreu <filabreu@gmail.com>
2014-08-07 22:24:20 -0300

    Interpreta arquivo TSV no model Sale. Cria método para transforma arquivo TSV em um array de hashes de atributos. Cria método para criar registros a partir de um array de hashes de atributos, atribuindo um usuário a cada registro.
    Signed-off-by: Filipe Abreu <filabreu@gmail.com>

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,22 @@
+require 'csv'
+
 class Sale < ActiveRecord::Base
   belongs_to :user
+
+  attr_accessor :file
+
+  def self.create_from_file(file, user)
+    file_data = parse_file(file)
+    self.create(file_data) do |sale|
+      sale.user = user
+    end
+  end
+
+  def self.parse_file(file)
+    data = CSV.read(file, { :col_sep => "\t" })
+    headers = data.shift.map { |header| header.parameterize.underscore.to_sym }
+    data.map do |sale_data|
+      headers.inject({}) { |hash, header| hash[header] = sale_data[headers.index(header)]; hash }
+    end
+  end
 end
